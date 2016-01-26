bd395429b7174c6f7e708a765be139a6920966d8
bwhyman <bwang0629@gmail.com>
2016-01-27 00:38:08 +0800

    marge

--- src/com/se/working/interceptor/UserInterceptor.java
+++ src/com/se/working/interceptor/UserInterceptor.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD:src/com/se/working/interceptor/UserInterceptor.java
 package com.se.working.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 /**
@@ -40,46 +38,3 @@ public class UserInterceptor implements HandlerInterceptor{
 	}
 
 }
-=======
-package com.se.working.interceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-/**
- * 登录操作验证
- * @author BO
- *
- */
-public class UserInterceptor implements HandlerInterceptor{
-
-	public UserInterceptor() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.out.println("UserInterecptor");
-		return true;
-	}
-
-}
->>>>>>> origin/master:src/com/se/working/interceptor/UserInterceptor.java
