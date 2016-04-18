fd58e5959e0484308a86f8caa4f204fe029afd27
bwhyman <bwang0629@gmail.com>
2016-04-18 22:58:53 +0800

    u
    u

--- src/com/se/working/interceptor/UserInterceptor.java
+++ src/com/se/working/interceptor/UserInterceptor.java
@@ -6,8 +6,53 @@ import javax.servlet.http.HttpServletResponse;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> origin/master
+=======
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 登录操作验证
+ * @author BO
+ *
+ */
+public class UserInterceptor implements HandlerInterceptor{
+
+	public UserInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("UserInterecptor");
+		return true;
+	}
+
+}
+=======
+package com.se.working.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+>>>>>>> parent of bd39542... marge
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
